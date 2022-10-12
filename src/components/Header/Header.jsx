fb1e77d05c7bf538bddf439d2da9fa03333a78f9
GuillermoTD <88951508+GuillermoTD@users.noreply.github.com>
2022-10-12 17:58:09 -0400

    primer commit para probar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Header from 'HeaderStyled';
+
+const Header = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/HeaderStyled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Header = styled.div`
+  background:red;
+  width:100%;
+  heigth:3rem;
+`;
+
+export default Header;
